a0f66e23eabb30349e4a7f5dbd0feccce4c016a4
phgiacomelli <paulo.giacomelli@aluno.feliz.ifrs.edu.br>
2023-04-03 18:54:26 -0300

    Fazendo a req apenas após apertar o botão

--- completion.js
+++ completion.js
@@ -1,25 +1,5 @@
 function explain_breed(breed) {
     // coloque sua api key aqui
-    const apiKey = "";
-    const prompt = `Explique sobre a raça ${breed}`;
-
-    fetch("https://api.openai.com/v1/engines/text-davinci-002/completions", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-            prompt: prompt,
-            max_tokens: 250,
-            top_p: 1,
-            temperature: 0
-        })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            return; 
-        })
-        .catch(error => console.error(error));
+    const apiKey = "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";
+    
 }
--- controller.js
+++ controller.js
@@ -1,17 +1,39 @@
 const explain_app = angular.module('explain-breed', []);
 
-explain_app.controller('ExplainController', ['$scope', function($scope) {
+explain_app.controller('ExplainController', ['$scope', function ($scope) {
     $scope.breed = '';
-    console.log($scope.breed);
 
-    $scope.explain_breed = function(breed) {
+
+    $scope.explain_breed = function (breed) {
         if (breed == null ||
             breed == "") {
-            show_message("Raça nao preenchida!", "error");         
+            show_message("Raça nao preenchida!", "error");
             return;
         }
-    
         console.log("agr vai");
+
+        const apiKey = "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";
+        const prompt = `Explique sobre a raça ${breed}`;
+
+        fetch("https://api.openai.com/v1/engines/text-davinci-002/completions", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            },
+            body: JSON.stringify({
+                prompt: prompt,
+                max_tokens: 250,
+                top_p: 1,
+                temperature: 0
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                return;
+            })
+            .catch(error => console.error(error));
 
     }
 }]);
